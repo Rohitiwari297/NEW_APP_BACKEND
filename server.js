8148e2c4348432fa94133fc7b7fce5cf47b9fdc1
Rohitiwari <tripathirohit297@gmail.com>
2026-06-05 18:29:05 +0530

    updated server.js file

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import app from './src/app.js'
 import config from './src/config/config.js'
 
 const port = config.PORT || 7878;
-
+//start the server
 console.log(port);
 app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on http://0.0.0.0:${port}`)

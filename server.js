ee09143437ef461d0eee086f4c0991257b593113
Rohitiwari <tripathirohit297@gmail.com>
2026-06-16 12:42:25 +0530

    updated docker ignore

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ import config from './src/config/config.js'
 const port = config.PORT || 7878;
 //start the server
 console.log(port);
+// 
 app.listen(port, "0.0.0.0", () => {
     console.log(`Server running on http://0.0.0.0:${port}`)
 })

97c9ecfdca28a1eed61c3a26b98c7c5080f4c3bf
Rohitiwari <tripathirohit297@gmail.com>
2026-06-26 19:05:59 +0530

    Give delete article permission to the editor

--- src/modules/article/article.controller.js
+++ src/modules/article/article.controller.js
@@ -654,7 +654,7 @@ export const deleteArticle = AsyncHandler(async (req, res) => {
 
 
 
-    if (![roleContaints.admin, roleContaints.reporter].includes(userRole)) {
+    if (![roleContaints.admin, roleContaints.reporter, roleContaints.editor].includes(userRole)) {
         throw new ApiError(403, "You are not Authorized to access this api");
     }
 
